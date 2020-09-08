c068750b1e233a6a574ed0000eca65c9009eb59b
Chris Troutner <chris.troutner@gmail.com>
2020-09-08 13:18:24 -0700

    Added client2

--- bin/client2/client2.js
+++ bin/client2/client2.js
@@ -6,7 +6,7 @@
 const MASTER_MULTIADDR = '/ip4/127.0.0.1/tcp/5004/p2p/Qma7XadmQ2LwVi6jkFtZJEBxDaNQwbyjj1TBQiUGxjeKrR'
 const ROOM_NAME = 'room-name'
 const MNEMONIC =
-  'creek caution crouch bid route gold prepare need above movie broom denial'
+  'flat bench boss forest weasel frost pear loan journey start wish unit'
 
 // Global npm libraries
 const Room = require('ipfs-pubsub-room')
@@ -26,9 +26,9 @@ const ipfsOptions = {
   },
   config: {
     Addresses: {
-      Swarm: ['/ip4/0.0.0.0/tcp/5104', '/ip4/190.198.70.169/tcp/5105/ws'],
-      API: '/ip4/127.0.0.1/tcp/5106',
-      Gateway: '/ip4/127.0.0.1/tcp/5107',
+      Swarm: ['/ip4/0.0.0.0/tcp/5204', '/ip4/190.198.70.169/tcp/5205/ws'],
+      API: '/ip4/127.0.0.1/tcp/5206',
+      Gateway: '/ip4/127.0.0.1/tcp/5207',
       Delegates: []
     }
   },
